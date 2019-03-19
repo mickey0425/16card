db647d6c4ee001b0d49da0daf0eebd361c520ae5
mickey <pandamickey0425@gmail.com>
2019-03-20 01:05:33 +0800

    random card ok

--- 16card/ViewController.swift
+++ 16card/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    
+    @IBOutlet var cardButton: [UIButton]!
+    
+   
+    var flipCount:Int = 0{
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    
+    var emojiChoise = ["🚷","🚯","🚳","🚱","🔞","📵","🚭","🆘","🔆","⚠️","✅","❇️","❌","🛑","❎","💀"]
+    
+//   var number = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButton.index(of: sender){
+            filpCard(withEmoji: emojiChoise[cardNumber], on : sender)
+        }else {
+            print("error")
+        }
+        flipCount += 1
+    
 }
+    
+    func filpCard(withEmoji emoji: String, on button:UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("",for:UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 
+    @IBAction func flipAll(_ sender: Any) {
+        emojiChoise.shuffle()
+        
+        var i:Int = 0
+        for button in cardButton {
+            
+                button.setTitle(emojiChoise[i],for:UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+                i += 1
+            }
+        
+        
+        flipCount += 16
+    }
+    
+    @IBAction func resetCard(_ sender: Any) {
+        
+        for button in cardButton {
+            button.setTitle("",for:UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            
+        }
+        flipCount = 0
+    }
+    
+    
+    
+}
